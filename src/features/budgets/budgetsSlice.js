ee39d61e01ce21deb4bd193604a6583b56d0bbd7
vadimP74 <mydom74@gmail.com>
2024-08-20 19:43:21 -0500

    remove reducer and add Slice in transactionSlice and budgetsSlice

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export const CATEGORIES = [
   "housing",
   "food",
@@ -14,27 +16,22 @@ const initialState = CATEGORIES.map((category) => ({
   amount: 0,
 }));
 
-export const editBudget = (budget) => {
-  return {
-    type: "budgets/editBudget",
-    payload: budget,
-  };
-};
-
-const budgetsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "budgets/editBudget":
-      const newBudgets = state.map((budget) => {
-        if (budget.category === action.payload.category) {
-          return action.payload;
-        }
-        return budget;
-      });
-      return newBudgets;
-    default:
-      return state;
+export const budgetsSlice = createSlice({
+  name: 'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      state.map((budget) => {
+        if (budget.category === action.payload.category) return budget.amount = action.payload.amount;
+        return budget
+      })
+    }
   }
-};
+})
+
+export const { editBudget } = budgetsSlice.actions;
 
 export const selectBudgets = (state) => state.budgets;
-export default budgetsReducer;
+
+export default budgetsSlice.reducer;
+
--- src/features/transactions/transactionsSlice.js
+++ src/features/transactions/transactionsSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export const CATEGORIES = [
   "housing",
   "food",
@@ -13,50 +15,23 @@ const initialState = Object.fromEntries(
   CATEGORIES.map((category) => [category, []])
 );
 
-export const addTransaction = (transaction) => {
-  return {
-    type: "transactions/addTransaction",
-    payload: transaction,
-  };
-};
-
-export const deleteTransaction = (transaction) => {
-  return {
-    type: "transactions/deleteTransaction",
-    payload: transaction,
-  };
-};
+export const transactionsSlice = createSlice({
+  name: 'transactions',
+  initialState: initialState,
+  reducers: {
+    addTransaction: (state, action) => {
+      state[action.payload.category].push(action.payload)
+    },
+    deleteTransaction: (state, action) => {
+      state[action.payload.category] = state[action.payload.category].filter(transaction => transaction.id !== action.payload.id)
+    }
+  }
+})
 
 export const selectTransactions = (state) => state.transactions;
 export const selectFlattenedTransactions = (state) =>
   Object.values(state.transactions).reduce((a, b) => [...a, ...b], []);
 
-const transactionsReducer = (state = initialState, action) => {
-  let newTransactionsForCategory;
-  switch (action.type) {
-    case "transactions/addTransaction":
-      newTransactionsForCategory = [
-        ...state[action.payload.category].slice(),
-        action.payload,
-      ];
-      return {
-        ...state,
-        [action.payload.category]: newTransactionsForCategory,
-      };
-    case "transactions/deleteTransaction":
-      const deletedIndex = state[action.payload.category].findIndex(
-        (transaction) => transaction.id === action.payload.id
-      );
-      newTransactionsForCategory = state[action.payload.category].filter(
-        (item, index) => index !== deletedIndex
-      );
-      return {
-        ...state,
-        [action.payload.category]: newTransactionsForCategory,
-      };
-    default:
-      return state;
-  }
-};
+export const { addTransaction, deleteTransaction } = transactionsSlice.actions;
 
-export default transactionsReducer;
+export default transactionsSlice.reducer;
